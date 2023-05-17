5dc1ef5e2d2f6c5953bb3fc39b647940ee490a3d
Herman Zakharov <h.zakharov21@gmail.com>
2023-05-18 01:30:44 +0300

    Small fix

--- Assets/Companion/Scripts/FollowingAndThrowCompanion.cs
+++ Assets/Companion/Scripts/FollowingAndThrowCompanion.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class FollowingAndThrowCompanion : MonoBehaviour
 {
+
+    public Rigidbody2D rb;
+    private Transform player;
+
+    public float speedOfFoll = 5f;      //—корость преследовани€
+
     private bool isCompanionThrown = false;
     public float CooldownTime = 5f;
-    public float speedOfFoll = 5f;                   //—корость преследовани€
-    public float speedOfThrow = 10f;
-    public Rigidbody2D rb;
-    Transform target;
+    public float speedOfThrow = 1f;
+    private Vector2 startPosition;
+    private Vector2 endPosition;
+    private float targetX = 0f;
 
-
-    private Transform startPosition;
-    private Transform endPosition;
-    private Transform targetToThrow;
-    
     // Start is called before the first frame update
     void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
+
+        startPosition = transform.position;
+        endPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -30,25 +34,32 @@ public class FollowingAndThrowCompanion : MonoBehaviour
         if (Input.GetButtonDown("Fire1"))
         {
             isCompanionThrown = true;
-            Vector2 startPosition = transform.position;
-            Vector2 endPosition = new Vector2(startPosition.x + 7, startPosition.y);
-            float distance = Vector2.Distance(startPosition, endPosition);
-            float timeOfThrow = distance / speedOfThrow;
+            targetX = 3f;
+            endPosition = new Vector2(startPosition.x + targetX, startPosition.y);
+            /* Vector2 startPosition = transform.position;
+             Vector2 endPosition = new Vector2(startPosition.x + 7, startPosition.y);
+             float distance = Vector2.Distance(startPosition, endPosition);
+             float timeOfThrow = distance / speedOfThrow;
 
-            Vector2 direction = (endPosition - startPosition).normalized;
-            rb.velocity = new Vector2(distance, direction.y);
+             Vector2 direction = (endPosition - startPosition).normalized;
+             rb.velocity = new Vector2(distance, direction.y);*/
 
         }
-        //¬от тут надо бы проверку isThrown забацать, чтобы он не бегал за игроком, пока ультит
-        //ћожно сделать все в одном файле, типа эту строку перенести на когда isThrown == false
-        //ј можно сделать публичный статический класс, в котором будут только переменные хранитьс€
-        //“ипа статов, хп, урон вс€ шл€па, и туда же впихнуть isThrown
-        //ј так как этот класс будет статичным, то к его переменным можно будет обращатьс€ отовсюду,
-        //» смотреть их и измен€ть
-        //¬от так как-то
+
+        if (isCompanionThrown)
+        {   
+            startPosition = transform.position;
+            transform.position = Vector2.MoveTowards(startPosition, endPosition, speedOfThrow * Time.deltaTime);
+
+            if (Vector2.Distance(transform.position, endPosition) < 0.2f)
+            {
+                isCompanionThrown = false;
+            }
+        }
+
         if (isCompanionThrown == false)
         {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speedOfFoll * Time.deltaTime);
+
         }
     }
 }
--- Assets/Player/Script/Ulti.cs
+++ Assets/Player/Script/Ulti.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class Ulti : MonoBehaviour
 {
-    public Transform firepoint;
-    public GameObject Companion;
+
     public Rigidbody2D rb;
-    public float FlySpeed = 1f;
+
+    public float speedOfThrow = 10f;
     //Переменная для фиксации состояния "Кастую ульту"
     private bool isThrown = false;
     private Vector2 startPosition;
     private Vector2 endPosition;
     private float targetX = 0f;
+    private float targetY = 0f;
 
- 
 
     private void Start()
     {
@@ -30,15 +30,15 @@ public class Ulti : MonoBehaviour
         {
             //Начинает кастовать
             isThrown = true;
+            targetX = 3f;
+            endPosition = new Vector2(startPosition.x + targetX, startPosition.y);
         }
         //Пока кастует..
         if (isThrown)
         {
             //Делает все вычисления, передвигается, всё чики пуки
             startPosition = transform.position;
-            targetX = 3f;
-            endPosition = new Vector2(startPosition.x + targetX, startPosition.y);
-            transform.position = Vector2.MoveTowards(startPosition, endPosition, FlySpeed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(startPosition, endPosition, speedOfThrow * Time.deltaTime);
 
             //И когда дистанция сократилась достаточно...
             if (Vector2.Distance(transform.position, endPosition) < 0.5f)

309edc1f1f9c46d247ae436cd83c433e4fb22c3b
DoomGuy1818 <zasdqs745@gmail.com>
2023-05-18 16:18:34 +0300

    Fixbugs

--- Assets/Companion/Scripts/FollowingAndThrowCompanion.cs
+++ Assets/Companion/Scripts/FollowingAndThrowCompanion.cs
@@ -5,74 +5,29 @@ using GoodiesForFollowing;
 
 public class FollowingAndThrowCompanion : MonoBehaviour
 {
-<<<<<<< Updated upstream
-
-    public Rigidbody2D rb;
-    private Transform player;
-
-    public float speedOfFoll = 5f;      //Скорость преследования
-
-    private bool isCompanionThrown = false;
-    public float CooldownTime = 5f;
-    public float speedOfThrow = 1f;
-    private Vector2 startPosition;
-    private Vector2 endPosition;
-    private float targetX = 0f;
-=======
     //private bool isCompanionThrown = false;
     //public float CooldownTime = 5f;
     public float speedOfFoll = 5f;                   //Скорость преследования
     //public float speedOfThrow = 10f;
     public Rigidbody2D rb;
     Transform target;
->>>>>>> Stashed changes
 
+
+    private Transform startPosition;
+    private Transform endPosition;
+    private Transform targetToThrow;
+    
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
-
-        startPosition = transform.position;
-        endPosition = transform.position;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
 
-<<<<<<< Updated upstream
-        if (Input.GetButtonDown("Fire1"))
-        {
-            isCompanionThrown = true;
-            targetX = 3f;
-            endPosition = new Vector2(startPosition.x + targetX, startPosition.y);
-            /* Vector2 startPosition = transform.position;
-             Vector2 endPosition = new Vector2(startPosition.x + 7, startPosition.y);
-             float distance = Vector2.Distance(startPosition, endPosition);
-             float timeOfThrow = distance / speedOfThrow;
-
-             Vector2 direction = (endPosition - startPosition).normalized;
-             rb.velocity = new Vector2(distance, direction.y);*/
-
-        }
-
-        if (isCompanionThrown)
-        {   
-            startPosition = transform.position;
-            transform.position = Vector2.MoveTowards(startPosition, endPosition, speedOfThrow * Time.deltaTime);
-
-            if (Vector2.Distance(transform.position, endPosition) < 0.2f)
-            {
-                isCompanionThrown = false;
-            }
-        }
-
-        if (isCompanionThrown == false)
-        {
-
-        }
-=======
         //if (Input.GetButtonDown("Fire1"))
         //{
         //    isCompanionThrown = true;
@@ -96,6 +51,5 @@ public class FollowingAndThrowCompanion : MonoBehaviour
         //{
             transform.position = Vector2.MoveTowards(transform.position, target.position, speedOfFoll * Time.deltaTime);
         //}
->>>>>>> Stashed changes
     }
 }
--- Assets/Player/Script/Ulti.cs
+++ Assets/Player/Script/Ulti.cs
@@ -4,24 +4,8 @@ using UnityEngine;
 
 namespace GoodiesForFollowing
 {
-<<<<<<< Updated upstream
-
-    public Rigidbody2D rb;
-
-    public float speedOfThrow = 10f;
-    //Переменная для фиксации состояния "Кастую ульту"
-    private bool isThrown = false;
-    private Vector2 startPosition;
-    private Vector2 endPosition;
-    private float targetX = 0f;
-    private float targetY = 0f;
-
-
-    private void Start()
-=======
 
     public class Ulti : MonoBehaviour
->>>>>>> Stashed changes
     {
         public Transform firepoint;
         public GameObject Companion;
@@ -37,26 +21,6 @@ namespace GoodiesForFollowing
 
         private void Start()
         {
-<<<<<<< Updated upstream
-            //Начинает кастовать
-            isThrown = true;
-            targetX = 3f;
-            endPosition = new Vector2(startPosition.x + targetX, startPosition.y);
-        }
-        //Пока кастует..
-        if (isThrown)
-        {
-            //Делает все вычисления, передвигается, всё чики пуки
-            startPosition = transform.position;
-            transform.position = Vector2.MoveTowards(startPosition, endPosition, speedOfThrow * Time.deltaTime);
-
-            //И когда дистанция сократилась достаточно...
-            if (Vector2.Distance(transform.position, endPosition) < 0.5f)
-            {
-                //То состояние "Кастую ульту" выключается
-                isThrown = false;
-            }
-=======
             startPosition = transform.position;
             endPosition = transform.position;
         }
@@ -89,7 +53,6 @@ namespace GoodiesForFollowing
             }
 
 
->>>>>>> Stashed changes
         }
 
     }
